bd20c75c4332291d7dc0caa53dfcf2abf7377ef0
Tony Kovanen <tonykovanen@hotmail.com>
2014-04-30 20:25:37 +0300

    Tiled Gallery: Remove unneeded function.
    The generate_carousel_image_args function is unnecessary in the plugin file, since it is already in found in the tiled gallery item class.

--- modules/tiled-gallery/tiled-gallery.php
+++ modules/tiled-gallery/tiled-gallery.php
@@ -195,47 +195,6 @@ class Jetpack_Tiled_Gallery {
 			->raw( "data-carousel-extra='" . json_encode( $extra_data ) . "'" );
 	}
 
-	function generate_carousel_image_args( $image ) {
-		$attachment_id = $image->ID;
-		$orig_file       = wp_get_attachment_url( $attachment_id );
-		$meta            = wp_get_attachment_metadata( $attachment_id );
-		$size            = isset( $meta['width'] ) ? intval( $meta['width'] ) . ',' . intval( $meta['height'] ) : '';
-		$img_meta        = ( ! empty( $meta['image_meta'] ) ) ? (array) $meta['image_meta'] : array();
-		$comments_opened = intval( comments_open( $attachment_id ) );
-
-		$medium_file_info = wp_get_attachment_image_src( $attachment_id, 'medium' );
-		$medium_file      = isset( $medium_file_info[0] ) ? $medium_file_info[0] : '';
-
-		$large_file_info  = wp_get_attachment_image_src( $attachment_id, 'large' );
-		$large_file       = isset( $large_file_info[0] ) ? $large_file_info[0] : '';
-		$attachment_title = wptexturize( $image->post_title );
-		$attachment_desc  = wpautop( wptexturize( $image->post_content ) );
-
-        // Not yet providing geo-data, need to "fuzzify" for privacy
-		if ( ! empty( $img_meta ) ) {
-            foreach ( $img_meta as $k => $v ) {
-                if ( 'latitude' == $k || 'longitude' == $k )
-                    unset( $img_meta[$k] );
-            }
-        }
-
-		$img_meta = json_encode( array_map( 'strval', $img_meta ) );
-
-		$output = sprintf(
-				'data-attachment-id="%1$d" data-orig-file="%2$s" data-orig-size="%3$s" data-comments-opened="%4$s" data-image-meta="%5$s" data-image-title="%6$s" data-image-description="%7$s" data-medium-file="%8$s" data-large-file="%9$s"',
-				esc_attr( $attachment_id ),
-				esc_url( wp_get_attachment_url( $attachment_id ) ),
-				esc_attr( $size ),
-				esc_attr( $comments_opened ),
-				esc_attr( $img_meta ),
-				esc_attr( $attachment_title ),
-				esc_attr( $attachment_desc ),
-				esc_url( $medium_file ),
-				esc_url( $large_file )
-			);
-		return $output;
-	}
-
 	public function gallery_classes() {
 		return 'tiled-gallery type-' . esc_attr( $this->atts['type'] ) . ' tiled-gallery-unresized';
 	}
